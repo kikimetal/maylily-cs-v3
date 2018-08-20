99b452dbe2017da5588c7183e7dffecb501d2e5e
kikimetal <reezafratzitz2@gmail.com>
2018-08-20 17:16:21 +0900

    props周りを改善。フォントテストも成功！

--- components/Card.vue
+++ components/Card.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div class="Card">
-    <h1>Card</h1>
-    <LazyLoadImg :filename="imgfilename" />
+    <h1 class="title">{{ title }}</h1>
+    <LazyLoadImg class="img" :filename="imgfilename" />
   </div>
 
 </template>
@@ -13,20 +13,31 @@ export default {
     LazyLoadImg,
   },
   props: {
-    imgfilename: { type: String, default: 'kikigirl.jpg' },
+    imgfilename: { type: [String, Number], default: 'adobe-stock__rose-aroma.jpg' },
+    title: { type: [String, Number], default: 'INSERT TITLE...' },
   },
 }
 </script>
 
 <style lang="scss" scoped>
+@import '~/assets/css/myset.scss';
+
 .Card{
+  font-family: FuturaBold;
   text-align: center;
-  color: #fff;
+  color: white;
   width: 90%;
-  background: thistle;
+  background: $primary;
   margin: 0 auto;
-  border-radius: 10px;
+  border-radius: $border-radius;
   box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.1);
   overflow: hidden;
+
+  .title{
+    line-height: 2.2;
+  }
+  .img{
+
+  }
 }
 </style>
