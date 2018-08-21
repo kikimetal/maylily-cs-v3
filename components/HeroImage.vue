5a3127b0014e4ab1bbf300ad09e895337f097955
kikimetal <reezafratzitz2@gmail.com>
2018-08-21 19:12:03 +0900

    - スタイルの最適化 - モバイルへの最適化 - 装飾を追加

--- components/HeroImage.vue
+++ components/HeroImage.vue
@@ -2,8 +2,12 @@
   <div :class="['HeroImage', $store.state.ww.size]">
 
     <img class="logo" src="~/assets/img/logo.png" />
-    <div class="background blur" />
-    <div class="background" />
+    <div class="background">
+      <span />
+      <span />
+      <span />
+      <span />
+    </div>
 
     <div class="heading">
       <h1 class="main">
@@ -12,7 +16,7 @@
         <span data-aos="fade-up" data-aos-offset="0" data-aos-delay="500" data-aos-duration="1200">from</span>
         <span data-aos="fade-up" data-aos-offset="0" data-aos-delay="600" data-aos-duration="1600">Fragrance</span>
       </h1>
-      <h2 class="sub">
+      <h2 class="sub" data-aos="fade-up" data-aos-offset="0" data-aos-delay="1700" data-aos-duration="700">
         <span>私たちはライフスタイルの創造を通じて、</span>
         <span>心豊かな社会の実現に貢献します。</span>
       </h2>
@@ -34,16 +38,17 @@ export default {
   position: relative;
   font-size: 45rem;
   width: 100%;
-  height: 95vh;
+  height: 88vh;
 
   .logo{
     position: absolute;
-    top: 8vh;
+    top: 5vh;
     left: 5vw;
-    width: calc(86px + 3vw);
+    width: calc(120px);
     height: auto;
     z-index: 2;
   }
+
   .background{
     overflow: visible;
     position: absolute;
@@ -52,33 +57,66 @@ export default {
     right: 0;
     bottom: 0;
     margin: auto;
-    width: 90%;
-    height: 60%;
+    width: 90vw;
+    height: 90vw;
     background: url('~/assets/img/adobe-stock__rose-aroma.jpg');
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
-    border-radius: $border-radius;
+    // border-radius: $border-radius;
+    box-shadow: 0 20px 70px 0 rgba($pinkgrey, 0.3);
     z-index: 1;
 
-    &.blur{
-      transform: translateY(20px);
-      filter: blur(70px);
-      opacity: 0.5;
-      z-index: 1;
+    & > span{
+      position: absolute;
+      display: block;
+      border-radius: 4px;
+      background: $lightgrey;
+      box-shadow: 0 20px 70px 0 rgba($pinkgrey, 0.8);
+
+      $weight: 3px;
+      $length: 62px;
+      $pos: 10px;
+
+      &:nth-child(1){
+        top: $pos;
+        left: -$pos;
+        width: $length;
+        height: $weight;
+      }
+      &:nth-child(2){
+        top: -$pos;
+        left: $pos;
+        width: $weight;
+        height: $length;
+      }
+      &:nth-child(3){
+        right: -$pos;
+        bottom: $pos;
+        width: $length;
+        height: $weight;
+      }
+      &:nth-child(4){
+        right: $pos;
+        bottom: -$pos;
+        width: $weight;
+        height: $length;
+      }
     }
   }
+
   .heading{
     overflow: visible;
     .main{
       overflow: visible;
       position: absolute;
-      top: auto;
+      top: 10vh;
       left: 0;
       right: 0;
-      bottom: calc(15vh + 0.7em);
+      bottom: 0;
       margin: auto;
       width: max-content;
+      height: max-content;
       max-width: 86%;
       text-align: left;
       color: $lightgrey;
@@ -99,13 +137,15 @@ export default {
       top: auto;
       left: 0;
       right: 0;
-      bottom: 7vh;
+      bottom: 1vh;
       text-align: center;
-      font-size: 0.25em;
+      font-size: 14rem;
       line-height: 2;
       color: $grey;
-      z-index: 3;
+      z-index: 4;
+
       span{
+        overflow: visible;
         display: inline-block;
         padding-bottom: 0.2em;
         line-height: 1;
@@ -113,12 +153,43 @@ export default {
     }
   }
 
-  &.md, &.lg, &.xl{
+  &.sm{
+    margin-bottom: 10vw;
+
+    .heading{
+      .sub{
+        margin: auto;
+        padding: 1em 0;
+        width: 90%;
+        background: $white;
+        box-shadow: 0 10px 40px 0 $shadow;
+      }
+    }
+  }
+
+  &.md, &.lg, &.xl{ // .HeroImage
     font-size: 60rem;
+    height: 90vh;
+
+    .logo{
+      top: 8vh;
+      left: 5vw;
+      width: calc(86px + 3vw);
+    }
+
+    .background{
+      height: 60%;
+    }
+
     .heading{
       .main{
-        text-align: right;
+        top: auto;
         bottom: calc(15vh + 0.7em);
+        text-align: right;
+      }
+      .sub{
+        bottom: 5vh;
+        font-size: 24rem;
       }
     }
   }
