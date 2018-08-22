01183389aea9d23343cf63aec7786574bceee978
kikimetal <reezafratzitz2@gmail.com>
2018-08-22 13:27:12 +0900

    グローバルナビのためのテーティング情報追加

---
+++ assets/routes.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    title: 'Home',
+    subtitle: 'トップ',
+    path: '/',
+    exact: true,
+  },
+  {
+    title: 'News',
+    subtitle: '最新の情報',
+    path: '/news',
+    exact: true,
+  },
+  {
+    title: 'About',
+    subtitle: '企業情報',
+    path: '/about',
+    exact: true,
+  },
+  {
+    title: 'Brand',
+    subtitle: 'ブランド事業',
+    path: '/brand',
+    exact: true,
+  },
+  {
+    title: 'Bulgarian Rose',
+    subtitle: 'ローズ原料事業',
+    path: '/bulgarian-rose',
+    exact: true,
+  },
+  {
+    title: 'OEM',
+    subtitle: 'フレグランスOEM事業',
+    path: '/oem',
+    exact: true,
+  },
+  {
+    title: 'Contact',
+    subtitle: 'お問い合わせ',
+    path: '/contact',
+    exact: true,
+  },
+]
