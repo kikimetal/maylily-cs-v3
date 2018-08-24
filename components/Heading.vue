cae0bde16ee28d181cdc5f1ccb364fa8b9fb62da
kikimetal <reezafratzitz2@gmail.com>
2018-08-24 21:57:24 +0900

    type を3つにわけて改修

--- components/Heading.vue
+++ components/Heading.vue
@@ -1,48 +1,144 @@
 <template lang="html">
-  <div class="Heading">
-    <h1 class="title">{{ title }}</h1>
-    <h2 class="subtitle">{{ subtitle }}</h2>
+  <div>
+
+    <!-- type photogenic -->
+    <div v-if="type === 'photogenic'" class="Heading photogenic">
+      <LazyLoadImg
+        :imgsrc="imgsrc"
+        :alt="alt" />
+      <!-- <h1 class="title">{{ title }}</h1> -->
+      <div class="title-container" data-aos="fade-up" data-aos-offset="0">
+        <h1 class="title">
+          <span v-for="(word, i) in titleArray" :key="'title-word-' + i">{{ word }}</span>
+        </h1>
+        <h2 class="subtitle">
+          <span v-for="(word, i) in subtitleArray" :key="'subtitle-word-' + i">{{ word }}</span>
+        </h2>
+      </div>
+    </div>
+
+    <!-- type big -->
+    <div v-else-if="type === 'big'" class="Heading big">
+      <h1>big</h1>
+      <h1 class="title">
+        <span v-for="(word, i) in titleArray" :key="'title-word-' + i">{{ word }}</span>
+      </h1>
+      <h2 class="subtitle">
+        <span v-for="(word, i) in subtitleArray" :key="'subtitle-word-' + i">{{ word }}</span>
+      </h2>
+    </div>
+
+    <!-- type basic -->
+    <div v-else class="Heading basic">
+      <h1 class="title">
+        <span v-for="(word, i) in titleArray" :key="'title-word-' + i">{{ word }}</span>
+      </h1>
+      <h2 class="subtitle">
+        <span v-for="(word, i) in subtitleArray" :key="'subtitle-word-' + i">{{ word }}</span>
+      </h2>
+    </div>
+
   </div>
 </template>
 
 <script>
+import LazyLoadImg from '~/components/LazyLoadImg.vue'
+
 export default {
-  props: {
-    title: { type: [String, Number], default: 'Heading...' },
-    subtitle: { type: [String, Number], default: 'sub heading...' },
+  components: {
+    LazyLoadImg,
   },
+  props: {
+    type: { type: String, default: '' },
+    title: { type: [String, Number, Array], default: 'Heading...' },
+    subtitle: { type: [String, Number, Array], default: 'sub heading...' },
+    imgsrc: { type: String, default: 'kikigirl.jpg' },
+    alt: { type: String, default: '画像です' },
+  },
+  data () {
+    return {
+      titleArray: typeof this.title === 'object' ? this.title : new Array(this.title),
+      subtitleArray: typeof this.subtitle === 'object' ? this.subtitle : new Array(this.subtitle),
+    }
+  },
+  created () {
+    if (process.browser) {
+      console.log(typeof this.imgsrc)
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '~/assets/css/myset.scss';
+
 .Heading{
-  font-size: 40rem;
-  padding: 8vw 4vw 4vw;
-  color: #333;
+  font-size: 24rem;
+  color: $general;
   text-align: left;
 
-  .title{
-    font-size: 1em;
-    padding: 0.1em 0 0.2em;
-    font-weight: bold;
-    word-wrap: break-word;
-  }
-  .subtitle{
-    font-size: 0.5em;
-    padding: 0;
-    font-weight: normal;
+  .title, .subtitle{
+    span{
+      display: inline-block;
+      padding-right: 0.2em;
+      &:last-child{
+        padding-right: 0;
+      }
+    }
   }
 
-  .title, .subtitle{
-    // text-transform: uppercase;
-    // background: $pink-gradient;
-    background: $primary-gradient;
-    width: max-content;
-    background-clip: text;
-    -webkit-background-clip: text;
-    // -webkit-text-fill-color: transparent;
-    color: transparent;
+  &.basic{
+    padding: 8vw 0 4vw;
+    .title{
+      font-size: 2em;
+      padding: 0.1em 0 0.2em;
+      font-weight: bold;
+      word-wrap: break-word;
+    }
+    .subtitle{
+      font-size: 0.9em;
+      padding: 0;
+      font-weight: normal;
+    }
+    .title, .subtitle{
+      span{
+        background: $primary-gradient;
+        background-clip: text;
+        -webkit-background-clip: text;
+        // -webkit-text-fill-color: transparent;
+        color: transparent;
+      }
+    }
+  }
+
+  &.big{
+
+  }
+
+  &.photogenic{
+    width: 100%;
+    text-align: center;
+    overflow: visible;
+    padding-bottom: 1em;
+    .title-container{
+      position: relative;
+      top: -1em;
+      margin: 0 auto;
+      padding: 1em 2em;
+      // width: max-content;
+      width: 80vw;
+      background: $white;
+      box-shadow: $shadow-set;
+      overflow: visible;
+    }
+    .title{
+      font-size: 30rem;
+      font-weight: bold;
+    }
+    .subtitle{
+      font-size: 15rem;
+      padding-top: 0.5em;
+    }
   }
 }
 
