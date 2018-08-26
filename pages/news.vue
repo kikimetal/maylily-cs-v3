e183547c6def14000e2acf9fc2e97974e08428c8
kikimetal <reezafratzitz2@gmail.com>
2018-08-26 21:44:55 +0900

    ダミーテキスト、メモなど削除。 sheet から データを取得して整形するとこまでほぼロジック完成。 API, queryString などの生成も保守しやすく改善。いい感じ。

--- pages/news.vue
+++ pages/news.vue
@@ -8,58 +8,18 @@
       subtitle="ニュースリリース"
       />
 
-    <!-- <CardContainer>
-      <Card
-        class="link"
-        v-for="(link, i) in sheet"
-        :key="'link-in-about-' + i"
-        :date="link.date"
-        :title="link['main-title-01']"
-        :subtitle="link['sub-title']"
-        :to="link['link-href']"
-        :exact="true"
-        :imgsrc="link['img-src']"
-        data-aos="fade-up"
-        :data-aos-delay="i * 100" />
-    </CardContainer> -->
     <CardContainer>
       <Card
-        v-for="(link, i) in sheet"
+        v-for="(row, i) in newsSheet"
         :key="'card-in-news-' + i"
-        type="modal"
-        :date="link.date"
-        :title="link['main-title-01']"
-        :subtitle="link['sub-title']"
-        :modal="{
-          title: link['main-title-01'],
-          text: [
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-            '詳細テキストです。', '詳細テキストです。', '詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。詳細テキストです。',
-          ],
-          links: [{}, {}],
-        }"
+        :type="row.type"
+        :date="row.date"
+        :title="row.title"
+        :subtitle="row.subtitle"
+        :align="row.align"
+        :imgsrc="row.imgsrc"
+        :modal="row.modal"
+        :exact="true"
         data-aos="fade-up"
         :data-aos-delay="i * 100" />
     </CardContainer>
@@ -82,52 +42,82 @@ export default {
     Card,
     CardContainer,
   },
+
   data () {
     return {
-      sheet: [],
-      linksInNews: [
-        {
-          title: 'vision',
-          subtitle: '社会貢献活動',
-          to: '/about/vision',
-          imgsrc: 'OurMission.jpg',
-          exact: true,
-        },
-        {
-          title: 'message',
-          subtitle: '社会貢献活動',
-          to: '/about/message',
-          imgsrc: 'OurMission.jpg',
-          exact: true,
-        },
-        {
-          title: 'outline',
-          subtitle: '社会貢献活動',
-          to: '/about/outline',
-          imgsrc: 'OurMission.jpg',
-          exact: true,
-        },
-        {
-          title: 'csr',
-          subtitle: '社会貢献活動',
-          to: '/about/csr',
-          imgsrc: 'OurMission.jpg',
-          exact: true,
-        },
-      ],
+      newsSheet: [],
     }
   },
+
   methods: {
     async fetchSheet () {
-      const url = "https://www.maylily.co.jp/v2/assets/gss-api.php?sheetName=news&date&main-title-01&main-title-02&sub-title&description&img-src&img-alt&link-flg&link-text&link-href"
-      let res = await axios.get(url)
+
+      const sheetName = 'news'
+      const columns = [
+        'separator',
+        'date',
+        'title',
+        'subtitle',
+        'align',
+        'imgsrc',
+        'type',
+        'modal-title',
+        'modal-subtitle',
+        'modal-text',
+        'modal-links-word',
+        'modal-links-url',
+      ]
+      const queryString = `?sheetName=${sheetName}&${columns.join('&')}`
+      const apiUrl = 'https://www.maylily.co.jp/v2/assets/gss-api.php'
+      const fetchUrl = apiUrl + queryString
+
+      let res = await axios.get(fetchUrl)
+
       if (!res.status === 200) {
         throw new Error(res.statusText)
       }
-      this.sheet = res.data
-      console.log(this.sheet)
+
+      const separator = res.data[0].separator
+      const fixedData = res.data.map(row => fixRow(row, separator))
+      this.newsSheet = fixedData
+      // console.log('news fixed data: ', fixedData) // TODO: remove
+
+      /**
+       * gss-apiから帰ってきた配列の1行分のデータを Card に対応するよう整形
+       * @param  { Object } row       1行分のデータ=1つのカード分の情報
+       * @param  { String } separator 文字列から配列に整形する際の区切る文字列
+       * @return { Object }           Card へ最適化したオブジェクト
+       */
+      function fixRow (row, separator) {
+        // generate links object.
+        const words = row['modal-links-word'].split(separator)
+        const urls = row['modal-links-url'].split(separator)
+        const links = words.map((word, i) => ({
+          word: word.trim(),
+          to: urls[i].trim() || '/',
+          external: urls[i].trim().indexOf('http') === 0
+        }))
+        // fixed object for type=modal Card.
+        return {
+          date: row.date.trim(),
+          title: row.title.trim().split(separator),
+          subtitle: row.subtitle.trim().split(separator),
+          align: row.align.trim() || 'left',
+          imgsrc: row.imgsrc.trim(),
+          // type は今は modal しかないけど、普通のリンクとかに拡張対応するときはここで変更
+          type: row.type.trim() || 'modal',
+          modal: {
+            title: row['modal-title'].trim().split(separator),
+            subtitle: row['modal-subtitle'].trim().split(separator),
+            text: row['modal-text'].trim().split(separator),
+            links: links,
+          },
+        }
+      } // function fix
+
     },
   },
+
   created () {
     if (process.browser) {
       this.fetchSheet()
