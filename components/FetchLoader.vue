dde9efd54364fb3bc6d14daa8aaa73af5b1b5ea3
kikimetal <reezafratzitz2@gmail.com>
2018-08-27 15:51:31 +0900

    コンポーネント追加

---
+++ components/FetchLoader.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <transition name="page">
+    <div
+      class="FetchLoader"
+      :style="{textAlign: 'center', fontWeight: 'bold', fontSize: '20px', color: 'grey'}"
+      v-if="$store.state.sheets.news.status === 'pending'"
+      >
+      <h1>LOADING...</h1>
+      <h2>最新の情報を読み込んでいます✨</h2>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/css/myset.scss';
+.FetchLoader{
+  font-size: 22rem;
+  text-align: center;
+  font-weight: bold;
+  background: $primary;
+  color: $white;
+}
+</style>
