14d450209053fd504817071a45f9f1861b47651c
kikimetal <reezafratzitz2@gmail.com>
2018-08-27 17:33:37 +0900

    Card を使ったローダーへ

--- components/FetchLoader.vue
+++ components/FetchLoader.vue
@@ -1,28 +1,60 @@
 <template lang="html">
   <transition name="page">
-    <div
-      class="FetchLoader"
-      :style="{textAlign: 'center', fontWeight: 'bold', fontSize: '20px', color: 'grey'}"
-      v-if="$store.state.sheets.news.status === 'pending'"
-      >
-      <h1>LOADING...</h1>
-      <h2>最新の情報を読み込んでいます✨</h2>
-    </div>
+    <CardContainer v-if="$store.state.sheets.news.status === 'pending'">
+      <Card
+        class="FetchLoader"
+        v-for="(row, i) in dummy"
+        :key="'card-in-news-' + i"
+        :date="row.date"
+        :title="row.title"
+        :subtitle="row.subtitle"
+        :align="row.align"
+        :imgsrc="row.imgsrc"
+        :type="row.type"
+        :to="row.linkto"
+        :modal="row.modal"
+        :exact="true"
+        data-aos="fade-up"
+        :data-aos-delay="i * 100" />
+    </CardContainer>
   </transition>
 </template>
 
 <script>
+import Card from '~/components/Card.vue'
+import CardContainer from '~/components/CardContainer.vue'
 export default {
+  components: {
+    Card,
+    CardContainer,
+  },
+  data () {
+    return {
+      dummy: Array(3).fill({
+        type: 'link',
+        linkto: '',
+        imgsrc: 'dummy.svg',
+        title: 'Now loading',
+        subtitle: '最新の情報を読み込んでいます✨',
+      }),
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '~/assets/css/myset.scss';
 .FetchLoader{
-  font-size: 22rem;
-  text-align: center;
-  font-weight: bold;
-  background: $primary;
-  color: $white;
+  // font-size: 22rem;
+  // text-align: center;
+  // font-weight: bold;
+  // background: $primary;
+  // color: $white;
+  //
+  // display: flex;
+  // flex-flow: column;
+  // justify-content: center;
+  // align-items: center;
+  // height: 100vh;
 }
 </style>
