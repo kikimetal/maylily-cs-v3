0275a337a50a8557b3858fa9471856164b6ff788
kikimetal <reezafratzitz2@gmail.com>
2018-08-29 21:53:47 +0900

    コンタクトフォーム追加

--- pages/contact/complete.vue
+++ pages/contact/complete.vue
@@ -8,7 +8,10 @@
       subtitle="お問い合わせ"
       />
 
-    <ContactForm />
+    <section>
+      <h3>問い合わせありがとうございます✨</h3>
+      <p>3営業日以内を目処に、入力されたメールアドレス宛にご返信いたします。</p>
+    </section>
 
     <Footer />
   </div>
@@ -17,12 +20,12 @@
 <script>
 import Heading from '~/components/Heading.vue'
 import Footer from '~/components/Footer.vue'
-import ContactForm from '~/components/ContactForm.vue'
+import MailForm from '~/components/MailForm.vue'
 export default {
   components: {
     Heading,
     Footer,
-    ContactForm,
+    MailForm,
   },
   head () { return this.$store.state.heads[this.$route.path] },
 }
@@ -30,5 +33,15 @@ export default {
 
 <style lang="scss" scoped>
 // @import '~/assets/css/myset.scss';
+.page{
+  text-align: center;
+  section{
+    min-height: 40vh;
+    padding-bottom: 10vh;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+  }
+}
 
 </style>
---
+++ pages/contact/index.vue
@@ -0,0 +1,34 @@
+<template>
+   <div :class="['page', $store.state.ww.size]">
+
+    <Heading
+      type="basic"
+      pagetop
+      :title="['Contact']"
+      subtitle="お問い合わせ"
+      />
+
+    <MailForm />
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Heading from '~/components/Heading.vue'
+import Footer from '~/components/Footer.vue'
+import MailForm from '~/components/MailForm.vue'
+export default {
+  components: {
+    Heading,
+    Footer,
+    MailForm,
+  },
+  head () { return this.$store.state.heads[this.$route.path] },
+}
+</script>
+
+<style lang="scss" scoped>
+// @import '~/assets/css/myset.scss';
+
+</style>
