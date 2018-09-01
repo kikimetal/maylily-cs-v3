88c1e8d95ab4bc9537723bcdb9d29db17508cc5d
kikimetal <reezafratzitz2@gmail.com>
2018-09-01 14:32:12 +0900

    ページテキスト追加

--- pages/about/csr.vue
+++ pages/about/csr.vue
@@ -6,19 +6,104 @@
       pagetop
       :title="['CSR']"
       subtitle="社会貢献活動"
-      />
+    />
 
-      <Footer />
+    <section>
+      <Heading
+      type="photogenic"
+      :title="['CSR']"
+      subtitle="社会貢献活動"
+      imgsrc="naming.jpg"
+      alt="企業理念のイメージ画像"
+      />
+      <p>
+        メイリリィは、本業を通じた社会貢献に加え、文化・芸術・教育・福祉の分野を中心に、会社の強みや技術、人材といった経営資源を活用して、積極的な社会貢献活動に取り組んでいます。
+      </p>
+      <p>
+        特に、ブルガリアンローズの普及、及び新しいブルガリアンローズ文化の創造・発信を目的に活動している、一般社団法人ブルガリアンローズ文化協会への全面的な支援を継続的に実施しております。
+      </p>
+    </section>
+
+    <section>
+      <Heading
+      type="photogenic"
+      :title="['Rose', 'Water']"
+      :subtitle="['ローズウォーター', 'レシピアワード']"
+      imgsrc="naming.jpg"
+      alt="企業理念のイメージ画像"
+      />
+      <h1>
+        「ローズウォーターレシピアワード」へ協賛
+      </h1>
+      <p>
+        ブルガリアンローズ文化協会のローズウォーター調味料の普及活動に賛同し、当協会が主催する「ローズウォーターレシピアワード」に、弊社製品を協賛しております。
+      </p>
+      <p>
+        当イベントでは、ローズウォーターを広く知っていただくために、全国の調理製菓専門学校の学生を対象に、ローズウォーターを使った独創的なレシピを募集し、競い合います。
+      </p>
+      <p>
+        ファイナリスト6名が参加する最終実技審査では、駐日ブルガリア大使、片岡護シェフ、土屋公二シェフによる厳しい審査が行われます。
+      </p>
+      <p>
+        これから社会人となる子どもたちは、食のプロである有名シェフの指導を通じて、プロフェッショナルとしての心構えや創意工夫の大切さを学びます。
+      </p>
+      <Btn
+      text="一般社団法人ブルガリアンローズ文化協会 公式サイト"
+      to="http://www.bulgarian-rose.or.jp/"
+      type="external"
+      data-aos="zoom-out"
+      data-aos-once="true"
+      />
+    </section>
+
+    <section>
+      <Heading
+      type="photogenic"
+      :title="['香楽', 'KOUGAKU']"
+      :subtitle="['香り創りによる', '情操教育を実施']"
+      imgsrc="naming.jpg"
+      alt="企業理念のイメージ画像"
+      />
+      <p>
+        「香楽KOUGAKU」とは、癒し（フレグランスセラピー）と教育（フレグランスアート）をテーマにして、代表・山下文江が考案した独自の香り創りメソッドです。
+      </p>
+      <p>
+        誰にでも楽しめる香り創りの香楽メソッドによって、嗅覚を使った情操教育を実践しています。
+      </p>
+      <p>
+        2011年より神奈川県立藤沢総合高校で、「香りを通して心を開く」というテーマの特別講座を行っております。
+      </p>
+      <p>
+        5日間の香楽メソッドプログラムを通じて、子どもたちがそれぞれの個性を発見し、多様な価値観を認め、豊かな感性を育んでいます。
+      </p>
+      <p>
+        <LazyLoadImg
+        imgsrc="OurVision.jpg"
+        />
+      </p>
+      <p>
+        <LazyLoadImg
+        imgsrc="OurVision.jpg"
+        />
+      </p>
+    </section>
+
+
+    <Footer />
   </div>
 </template>
 
 <script>
 import Heading from '~/components/Heading.vue'
 import Footer from '~/components/Footer.vue'
+import Btn from '~/components/Btn.vue'
+import LazyLoadImg from '~/components/LazyLoadImg.vue'
 export default {
   components: {
     Heading,
     Footer,
+    Btn,
+    LazyLoadImg,
   },
   head () { return this.$store.state.heads[this.$route.path] },
 }
--- pages/oem.vue
+++ pages/oem.vue
@@ -2,11 +2,216 @@
    <div :class="['page', $store.state.ww.size]">
 
     <Heading
-      type="basic"
-      pagetop
+    type="basic"
+    pagetop
+    :title="['Fragrance', 'OEM']"
+    :subtitle="['フレグランス', 'OEM事業']"
+    />
+
+    <section>
+      <Heading
+      type="photogenic"
       :title="['Fragrance', 'OEM']"
-      subtitle="フレグランスOEM事業"
+      :subtitle="['フレグランス', 'OEM事業']"
+      imgsrc="Delicious.jpg"
+      alt="ダブル蒸留法"
       />
+      <p>
+        メイリリィは、フレグランス関連製品の受託製造を行っております。弊社フレグランスデザイナーが、お客様のイメージに合わせた香りを提案いたします。
+      </p>
+      <p>
+        自社工場ならではの迅速な対応で、多品種・小ロットにも対応可能です。今までに培った実績やノウハウをもとに、処方開発から生産、品質保証までサポートし、お客様のご要望に合わせたオリジナル製品を提供いたします。
+      </p>
+    </section>
+
+    <section>
+      <h1>
+        メイリリィの強み
+      </h1>
+      <Heading
+      type="photogenic"
+      :title="['Point 1']"
+      :subtitle="['香りのプロフェッショナル']"
+      imgsrc="Delicious.jpg"
+      alt="ダブル蒸留法"
+      />
+      <h1>私たちは香りのプロフェッショナルです。</h1>
+      <p>
+        フレグランスの香り創りでは、香料の化学的知識や技術だけでなく、抽象的なイメージを香りで表現する芸術的な感性が必要です。香料の特性を十分理解した熟練のフレグランスデザイナーが、お客様のご要望にお応えするオリジナルの香りを提案いたします。
+      </p>
+      <Heading
+      type="photogenic"
+      :title="['Point 2']"
+      :subtitle="['自社工場 多品種・小ロット対応']"
+      imgsrc="Delicious.jpg"
+      alt="ダブル蒸留法"
+      />
+      <h1>
+        自社工場のため、多品種・小ロットに対応できます。
+      </h1>
+      <p>
+        化粧品製造業許可を取得した自社の化粧品工場で、受託製品を製造いたします。
+        自社工場ならではの臨機応変な対応と、価格を抑えた製品提案が可能です。
+        また、柔軟な製造体制を敷いているため、多品種、小ロットへの対応を得意としています。
+      </p>
+    </section>
+
+    <section>
+      <h1>製造品目</h1>
+      <p>
+        香水、ボディミスト、ハンドクリームなどの化粧品から、リードディフューザー、ファブリックミストなどの雑貨品まで幅広く承っております。また、化粧品、トイレタリーなどに用いられる調合香料（香粧品香料）も提供しております。その他、ローションやジェルなどのスキンケア・ボディケア用品の製造も可能です。
+      </p>
+      <div class="half">
+        <Heading
+        type="photogenic"
+        :title="['Fragrance']"
+        :subtitle="['香水']"
+        imgsrc="stock01.jpg"
+        alt="香水の画像"
+        />
+        <Heading
+        type="photogenic"
+        :title="['Reed', 'Diffuser']"
+        :subtitle="['リードディフューザー']"
+        imgsrc="stock02.jpg"
+        alt="香水の画像"
+        />
+        <Heading
+        type="photogenic"
+        :title="['Room', 'Mist']"
+        :subtitle="['ルームミスト']"
+        imgsrc="stock03.jpg"
+        alt="香水の画像"
+        />
+        <Heading
+        type="photogenic"
+        :title="['Fabric', 'Mist']"
+        :subtitle="['ファブリックミスト']"
+        imgsrc="stock01.jpg"
+        alt="香水の画像"
+        />
+        <Heading
+        type="photogenic"
+        :title="['Fragrance', 'Oil']"
+        :subtitle="['フレグランスオイル']"
+        imgsrc="stock02.jpg"
+        alt="香水の画像"
+        />
+        <Heading
+        type="photogenic"
+        :title="['Body', 'Mist']"
+        :subtitle="['ボディミスト']"
+        imgsrc="stock03.jpg"
+        alt="香水の画像"
+        />
+        <Heading
+        type="photogenic"
+        :title="['Hand', 'Cream']"
+        :subtitle="['ハンドクリーム']"
+        imgsrc="stock01.jpg"
+        alt="香水の画像"
+        />
+        <Heading
+        type="photogenic"
+        :title="['Cosmetics', 'Fragrance']"
+        :subtitle="['香粧品香料']"
+        imgsrc="stock02.jpg"
+        alt="香水の画像"
+        />
+      </div>
+    </section>
+
+    <section>
+      <h2>製作事例 オリジナル香水</h2>
+      <p class="grid">
+        <span>数量</span><span>100pcs</span>
+        <span>単価</span><span>2,200円～</span>
+        <span>納期</span><span>2ヶ月～</span>
+        <span>仕様</span><span>弊社有り型ボトル・キャップ<br/>ボトル1色印刷／化粧箱1色印刷</span>
+      </p>
+      <h2>製作事例 ファブリックミスト50ml</h2>
+      <p class="grid">
+        <span>数量</span><span>1,000pcs</span>
+        <span>単価</span><span>700円～</span>
+        <span>納期</span><span>2ヶ月～</span>
+        <span>仕様</span><span>弊社有り型ボトル・キャップ<br/>ボトル1色印刷／化粧箱1色印刷</span>
+      </p>
+
+      <h1>
+        製造の流れ
+      </h1>
+
+      <div class="half">
+        <div>
+          <Heading
+          type="photogenic"
+          :title="['STEP 6']"
+          :subtitle="['香粧品香料']"
+          imgsrc="stock01.jpg"
+          alt="香水の画像"
+          />
+          <h2>STEP１　ヒアリング</h2>
+          <p>製品コンセプトや香りのイメージなど、お客様のご要望をお伺いいたします。</p>
+        </div>
+
+        <div>
+          <Heading
+          type="photogenic"
+          :title="['STEP 6']"
+          :subtitle="['香粧品香料']"
+          imgsrc="stock02.jpg"
+          alt="香水の画像"
+          />
+          <h2>STEP２　試作・仕様選定</h2>
+          <p>ご要望に合わせた処方でサンプルを作成。容器・化粧箱の仕様やデザインの選定も行います。</p>
+        </div>
+        <div>
+          <Heading
+          type="photogenic"
+          :title="['STEP 6']"
+          :subtitle="['香粧品香料']"
+          imgsrc="stock03.jpg"
+          alt="香水の画像"
+          />
+          <h2>STEP３　見積・契約</h2>
+          <p>製品仕様の確定後、御見積書を提出。正式にご契約・ご発注をいただきます。</p>
+        </div>
+        <div>
+          <Heading
+          type="photogenic"
+          :title="['STEP 6']"
+          :subtitle="['香粧品香料']"
+          imgsrc="stock01.jpg"
+          alt="香水の画像"
+          />
+          <h2>STEP４　薬事申請</h2>
+          <p>薬事法に基づく販売名申請や、容器・包装資材表示の諸法令適合チェックを行います。</p>
+        </div>
+        <div>
+          <Heading
+          type="photogenic"
+          :title="['STEP 6']"
+          :subtitle="['香粧品香料']"
+          imgsrc="stock02.jpg"
+          alt="香水の画像"
+          />
+          <h2>STEP５　製造</h2>
+          <p>自社工場にて徹底した品質管理のもと、安心安全な製品を製造いたします。</p>
+        </div>
+        <div>
+          <Heading
+          type="photogenic"
+          :title="['STEP 6']"
+          :subtitle="['香粧品香料']"
+          imgsrc="stock03.jpg"
+          alt="香水の画像"
+          />
+          <h2>STEP６　検査・出荷</h2>
+          <p>検品後、完成した製品を国内ご指定先までお届けいたします。</p>
+        </div>
+      </div><!-- .half -->
+
+    </section>
 
     <Footer />
   </div>
