a3910fc3828d3836b32572ab9dc0dcb3502241c6
kikimetal <reezafratzitz2@gmail.com>
2018-09-08 21:20:48 +0900

    グローバルナビに使うRoutesを整理。フッターにのみ載せるものとの分離。

---
+++ assets/routes/footer.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: 'Terms Of Use',
+    subtitle: '利用規約',
+    path: '/terms-of-use',
+    exact: true,
+  },
+  {
+    title: 'Privacy Policy',
+    subtitle: '個人情報保護方針',
+    path: '/privacy-policy',
+    exact: true,
+  },
+]
