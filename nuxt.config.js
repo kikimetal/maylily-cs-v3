0925fce2674d1c0eb5736c7e2d7bb183f16eba03
kikimetal <reezafratzitz2@gmail.com>
2018-10-01 13:33:44 +0900

    iOSスタンドアロンの設定追加 google font noto sans JP を最適化

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,13 +10,15 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' },
       { hid: 'description', name: 'description', content: '有限会社メイリリィは、「衣・食・住・健・美」の生活文化を事業領域に定め、今までにない新しい価値やライフスタイルの創造を通じて、世界中のあらゆる人々の生活に幸せと彩りを届け、心豊かな社会の実現に貢献します。' },
-      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
       { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
     ],
     link: [
       { rel: 'icon', type: 'image/vnd.microsoft.icon', href: '/favicon.ico' },
       { rel: 'shortcut icon', type: 'image/vnd.microsoft.icon', href: '/favicon.ico' },
       { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-touch-icon.png' },
+      { rel: 'apple-touch-startup-image', href: '/apple-touch-icon.png' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+JP' },
     ]
   },
   /*
